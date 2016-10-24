3e2071157b3bb7dbf7eae27002742c9a0ed12de4
SanjithKanagavel <sanjithkanagavel@gmail.com>
2016-10-24 22:56:59 +0530

    .

--- index.js
+++ index.js
@@ -11,38 +11,17 @@ app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.send('Hello folks, I am a tracker bot who tracks your tasks.')
 })
-firebase.initializeApp({
-  serviceAccount: {
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_MAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY
-  },
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-});
-
-let db = firebase.database();
-firebase.database().ref('/').set({
-username: "test",
-email: "test@mail.com"
-});
-let ref = db.ref("server/saving-data/fireblog");
-var usersRef = ref.child("users");
-usersRef.set({
-  alanisawesome: {
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing"
-  },
-  gracehop: {
-    date_of_birth: "December 9, 1906",
-    full_name: "Grace Hopper"
-  }
-}, function(error) {
-    if (error) {
-      console.log("Data could not be saved." + error);
-    } else {
-      console.log("Data saved successfully.");
-    }
+function firebaseInit() {
+  firebase.initializeApp({
+    serviceAccount: {
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_MAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+    },
+    databaseURL: process.env.FIREBASE_DATABASE_URL
   });
+  console.log("Firebase Initialized")
+}
 
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
@@ -79,4 +58,15 @@ function sendTextMessage(sender, text) {
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
+    firebaseInit();
+    firebase.database().ref('/').set({
+    username: "test",
+    email: "test@mail.com"
+    }, function(error) {
+        if (error) {
+          console.log("Data could not be saved." + error);
+        } else {
+          console.log("Data saved successfully.");
+        }
+      });
 })
