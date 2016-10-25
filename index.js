8a5a9383a42eb0fd8e0b5605d62eb1af22ebb3b2
SanjithKanagavel <sanjithkanagavel@gmail.com>
2016-10-25 11:41:16 +0530

    Added Generic messages

--- index.js
+++ index.js
@@ -5,6 +5,43 @@ const request = require('request')
 const app = express()
 const firebase = require("firebase")
 const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+let generalMessage = {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "generic",
+            "elements": [{
+                "title": "Create your task",
+                "subtitle": "Creating task is simple",
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/4/47/Press_button_Wikimedia_CH.svg",
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Create Task",
+                    "payload": "create_task",
+                }],
+            }, {
+                "title": "View Tasks",
+                "subtitle": "View your task",
+                "image_url": "http://www.pd4pic.com/images/icon-folder-open-button-documents-document.png",
+                "buttons": [{
+                    "type": "postback",
+                    "title": "View Task",
+                    "payload": "view_task",
+                }],
+            },{
+                "title": "Delete Task",
+                "subtitle": "Delete your task",
+                "image_url": "http://blogs.cs.st-andrews.ac.uk/routing-island/files/2012/03/4-Delete.png",
+                "buttons": [{
+                    "type": "postback",
+                    "title": "Delete Task",
+                    "payload": "delete_task",
+                }],
+            }]
+        }
+    }
+}
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -54,12 +91,15 @@ app.post('/webhook/', function (req, res) {
       }
       if (event.postback) {
         let text = JSON.stringify(event.postback)
-        if(text == "create_task") {
-          sendTextMessage(sender, "Great! Lets create them now.", token)
-        } else if(text == "view_task") {
-          sendTextMessage(sender, "Great! Lets view them now.", token)
-        }
-        continue
+          if(text == "create_task") {
+            sendTextMessage(sender, "Great! Lets create them now.", token)
+          } else if(text == "view_task") {
+            sendTextMessage(sender, "Great! Lets view them now.", token)
+          }
+          else {
+            sendTextMessage(sender,text,token)
+          }
+          continue
       }
     }
     res.sendStatus(200)
@@ -84,49 +124,13 @@ function sendTextMessage(sender, text) {
     })
 }
 function sendGenericMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Create your task",
-                    "subtitle": "Creating task is simple",
-                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/4/47/Press_button_Wikimedia_CH.svg",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Create Task",
-                        "payload": "create_task",
-                    }],
-                }, {
-                    "title": "View Tasks",
-                    "subtitle": "View your task",
-                    "image_url": "http://www.pd4pic.com/images/icon-folder-open-button-documents-document.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "View Task",
-                        "payload": "view_task",
-                    }],
-                },{
-                    "title": "Delete Task",
-                    "subtitle": "Delete your task",
-                    "image_url": "http://blogs.cs.st-andrews.ac.uk/routing-island/files/2012/03/4-Delete.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Delete Task",
-                        "payload": "delete_task",
-                    }],
-                }]
-            }
-        }
-    }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
         method: 'POST',
         json: {
             recipient: {id:sender},
-            message: messageData,
+            message: generalMessage,
         }
     }, function(error, response, body) {
         if (error) {
