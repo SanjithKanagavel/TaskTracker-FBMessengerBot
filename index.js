83b2c416bfde7ebe5e2df59a8fb197d4a3a58524
SanjithKanagavel <sanjithkanagavel@gmail.com>
2016-10-25 14:00:50 +0530

    Testing automatedated reply

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express()
 const firebase = require("firebase")
 const token = process.env.FB_PAGE_ACCESS_TOKEN
 
+/*
+* Payload for generic messages
+*/
+
 let generalMessage = {
     "attachment": {
         "type": "template",
@@ -89,77 +93,55 @@ let viewTask = {
     }
 }
 
+/*
+* Firebase Initialization
+*/
+firebase.initializeApp({
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+})
+
+/*
+* App server Startup settings
+*/
+
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.get('/', function (req, res) {
     res.send('Hello folks, I am a tracker bot who tracks your tasks.')
 })
-
-firebase.initializeApp({
-    databaseURL: process.env.FIREBASE_DATABASE_URL
-});
-
-/*app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            let ts = event.timestamp
-            console.log(req.body.entry);
-            sendTextMessage(sender,text.substring(0, 200))
-            firebase.database().ref('/Chats/'+sender+"/"+ts).set({
-            "message" : text
-            }, function(error) {
-                if (error) {
-                  console.log("Data could not be saved." + error);
-                } else {
-                  console.log("Data saved successfully.");
-                }
-              });
-        }
-    }
-    res.sendStatus(200)
-})*/
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
-
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
       if (event.message && event.message.text) {
         let text = event.message.text
-        if (text === 'Generic') {
-            sendGenericMessage(sender,0)
-            continue
+        if(text.startsWith("add:")) {
+          //new task creation
+        } else if (text.startsWith("view:")) {
+          //view task section
         }
-        sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        else if (text.startsWith("del:")) {
+          //delete task section
+        } else {
+          sendGenericMessage(sender,0)
+          continue
+        }
       }
 
       if (event.postback) {
         let text = JSON.stringify(event.postback)
         let payload = event.postback.payload
           if(payload == "create_task") {
-            sendTextMessage(sender, "Type in the below format to create a task.\ndescription-dd/mm/yyy\ndescription-today\ndescription-tomorrow\n", token)
-            //sendGenericMessage(sender,1)
+            sendTextMessage(sender, "To add a task, message in this format.\nadd:description-dd/mm/yyy\nadd:description-today\nadd:description-tomorrow\n", token)
           } else if(payload == "view_task") {
-            //sendTextMessage(sender, "Great! Lets view them now.", token)
-            sendGenericMessage(sender,2)
+            sendTextMessage(sender, "To view a task, message in this format.\n'view:today' - Display Todays task\n'view:upcoming' - Display Upcoming Task\n'view:completed' - Display Completed Tasks\n", token)
           } else if(payload == "delete_task") {
-            sendTextMessage(sender, "Great! Lets view them now.", token)
-            sendGenericMessage(sender,3)
-          } else if(payload == "desc") {
-            sendTextMessage(sender, "Great! Lets view them now.", token)
-          } else if(payload == "date_time") {
-
-          } else if(payload == "today_task") {
-            sendTextMessage(sender, "Today Task.", token)
-          } else if(payload == "upcoming_task") {
-            sendTextMessage(sender, "Upcoming Task.", token)
-          } else if(payload == "recent_task") {
-            sendTextMessage(sender, "Recent Task.", token)
+            sendTextMessage(sender, "To delete a task, message in this format.\n'del:all ' - To delete all tasks\n'del:dd/mm/yyy dd/mm/yyy' - To delete tasks between two range\n'del:dd/mm/yyyy hh:mm' - To delete specific task in particular time\n", token)
           }
           continue
       }
@@ -167,6 +149,10 @@ app.post('/webhook/', function (req, res) {
     }
     res.sendStatus(200)
   })
+
+/*
+* Function for sending normal text messages
+*/
 
 function sendTextMessage(sender, text) {
     let messageData = { text:text }
@@ -186,6 +172,11 @@ function sendTextMessage(sender, text) {
         }
     })
 }
+
+/*
+* Function for sending generic text messages
+*/
+
 function sendGenericMessage(sender,index) {
   let messageData;
   if(index == 0) {
@@ -213,10 +204,27 @@ function sendGenericMessage(sender,index) {
         }
     })
 }
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-    for ( var i =0 ; i<10 ; i++ ) {
-      sendTextMessage(1108340685901481,"Hi");
+
+/*app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            let ts = event.timestamp
+            console.log(req.body.entry);
+            sendTextMessage(sender,text.substring(0, 200))
+            firebase.database().ref('/Chats/'+sender+"/"+ts).set({
+            "message" : text
+            }, function(error) {
+                if (error) {
+                  console.log("Data could not be saved." + error);
+                } else {
+                  console.log("Data saved successfully.");
+                }
+              });
+        }
     }
-})
+    res.sendStatus(200)
+})*/
